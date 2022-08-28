060adc750a641351f4aefa5a3f5a6f6d609636af
TiagoKl <ftiago7303@gmail.com>
2022-08-28 14:48:46 -0500

    Cawna Grupo

---
+++ plugins/nv-global.js
@@ -0,0 +1,16 @@
+let handler = m => m
+handler.all = async function (m) {
+let chat = global.db.data.chats[m.chat]
+
+if (/^hola$/i.test(m.text) && chat.audios && !chat.isBanned) {
+let vn = './media/Hola.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
+
+if (!chat.isBanned && chat.audios && m.text.match(/(anadieleimporta|a nadie le importa)/gi)) {
+let vn = './media/dylan1.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+   
+return !0 }
+export default handler
