ed383c6cfa498f9b99eee15c14dfe59176d4dbfb
Tiago <104955231+TiagoKl@users.noreply.github.com>
2022-08-29 18:40:46 -0500

    『卍』 𝙎𝙧 𝘾𝙖𝙬𝙣𝙖 𝘽𝙤𝙩 🍷

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,91 +1,34 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
-let fs = require('fs')
-let path = require('path')
-let { MessageType } = require('@adiwajshing/baileys')
-let PhoneNumber = require('awesome-phonenumber')
-let levelling = require('../lib/levelling')
+import fs from 'fs'
 let handler = async (m, { conn, usedPrefix }) => {
-let user = global.db.data.users[m.sender]
 let pp = './Menu2.jpg'
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-try {
-} catch (e) {
-} finally {
-let { name, limit, exp, banned, lastclaim, registered, regTime, age, level } = global.db.data.users[m.sender]
-let { min, xp, max } = levelling.xpRange(level, global.multiplier)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let username = conn.getName(who)
 let menu = `
-╭══〘 ✯✯✯✯✯✯✯✯ 〙═╮
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-║➤ *✨𝗛ola, ${taguser} :D*
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-╰══╡✯✯✯✯✯✯✯✯╞══╯
-┏━━━━━━━━━━━━━┓
-┃ *< COMANDOS >*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-┣ ඬ⃟💫 _a_
-┣ ඬ⃟💫 _${usedPrefix}cat_
-┣ ඬ⃟💫 _${usedPrefix}dog_
-┣ ඬ⃟💫 _${usedPrefix}meme_
-┣ ඬ⃟💫 _${usedPrefix}logos_
-┣ ඬ⃟💫 _${usedPrefix}runtime_
-┣ ඬ⃟💫 _${usedPrefix}infohost_
-┣ ඬ⃟💫 _${usedPrefix}on welcome_
-┣ ඬ⃟💫 _${usedPrefix}off welcome_
-┣ ඬ⃟💫 _${usedPrefix}mediafire *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}tiktok *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}sticker *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}attp *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}attp2 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}attp3 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp2 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp3 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp4 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp5 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}wikipedia *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}google *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}imagen *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}play *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}play2 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}play3 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}invocar *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ytsearch *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}getaud *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}getvid *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}ytmp3 *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}ytmp4 *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}tts *[lenguaje] [texto]*_
-┣ ඬ⃟💫 _${usedPrefix}toimg *[sticker]*_
-┣ ඬ⃟💫 _${usedPrefix}sticker *[imagen]*_
-┣ ඬ⃟💫 _${usedPrefix}tourl *[imagen]*_
-┣ ඬ⃟💫 _${usedPrefix}tourl *[video]*_
-┣ ඬ⃟💫 _${usedPrefix}tourl *[audio]*_
-┣ ඬ⃟💫 _${usedPrefix}fat *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}bass *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}blown *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}deep *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}fast *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}robot *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}slow *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}tupai *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}vibra *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}nightcore *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}earrape *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}reverse *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}smooth *[nota de voz]*_
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-┃ *< OWNER >*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-┣ ඬ⃟💫 _${usedPrefix}update_
-┗━━━━━━━━━━━━━┛
+ ${taguser} 
+
+◌ » ———— ╫ ❲ ❈ ❳ ╫ ———— « ◌
+      𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐏𝐀𝐑𝐀 𝐆𝐑𝐔𝐏𝐎𝐒
+
+⚙️ _${usedPrefix}add *<numero>*_ 
+ ⚙️ _${usedPrefix}kick *<@tag>*_ 
+ ⚙️ _${usedPrefix}grupo *<abrir / cerrar>*_ 
+ ⚙️ _${usedPrefix}promote *<@tag>*_ 
+ ⚙️ _${usedPrefix}demote *<@tag>*_ 
+ ⚙️ _admins *<texto>*_ (Uso sin prefijo) 
+ ⚙️ _${usedPrefix}demote *<@tag>*_ 
+ ⚙️ _${usedPrefix}infogroup_ 
+ ⚙️ _${usedPrefix}link_ 
+ ⚙️ _${usedPrefix}setname *<texto>*_ 
+ ⚙️ _${usedPrefix}setdesc *<texto>*_ 
+ ⚙️ _${usedPrefix}invocar *<texto>*_ 
+ ⚙️ _${usedPrefix}setwelcome *<texto>*_ 
+ ⚙️ _${usedPrefix}setbye *<texto>*_ 
+ ⚙️ _${usedPrefix}hidetag *<texto>*_ 
+ ⚙️ _${usedPrefix}fantasmas_
+◌ » ———— ╫ ❲ ❈ ❳ ╫ ———— « ◌
+
 `.trim()
-//let mentionedJid = [who]
 let buttons = [
-{ buttonId: '#owner', buttonText: { displayText: '💫 𝐎𝐖𝐍𝐄𝐑 💫' }, type: 1 },
-{ buttonId: '#runtime', buttonText: { displayText: '⏰ 𝐑𝐔𝐍𝐓𝐈𝐌𝐄 ⏰' }, type: 1 },
-{ buttonId: '#infohost', buttonText: { displayText: '👑 𝐈𝐍𝐅𝐎𝐇𝐎𝐒𝐓 👑' }, type: 1 }]
+{ buttonId: 'uuu', buttonText: { displayText: '𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死' }, type: 1 }]
 let buttonMessage = {
 image: fs.readFileSync('./Menu2.jpg'),
 caption: menu.trim(),
@@ -99,15 +42,14 @@ externalAdReply: {
 showAdAttribution: true,
 mediaType: 'VIDEO',
 mediaUrl: null,
-title: '👑 𝐀𝐜𝐢𝐝𝐢𝐜𝐍𝐨𝐝𝐞𝐬 𝐇𝐨𝐬𝐭 👑',
+title: '𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死',
 body: null,
 thumbnail: fs.readFileSync('./src/logo.png'),
-sourceUrl: `https://chat.whatsapp.com/F0fU7LSlBBcBm6ny5fVSuT`
+sourceUrl: `https://chat.whatsapp.com/Dbw92638r4zGsRnUcoAbeU`
 }}}
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-}}
-handler.help = ['menu', 'help', '?']
-handler.tags = ['general']
+}
 handler.command = /^(menucompleto|comandos|allmenu|info|speed|estado|menú|menu|help|\?)$/i
+handler.admin = true
 handler.fail = null
-module.exports = handler
+export default handler
