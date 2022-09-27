584595ae08ac1a31c4e4436eb34bf02cc1b9d9b7
Cawna.sex <104955231+TiagoKl@users.noreply.github.com>
2022-09-27 14:22:50 -0500

    𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -2,7 +2,7 @@ let handler = m => m
 handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
 
-if (/^Bot|BOT|bot$/i.test(m.text) && chat.audios && !chat.isBanned) {
+if (/^Bot|BOT|Cawna|cawna|bot$/i.test(m.text) && chat.audios && !chat.isBanned) {
 let vn = './audio/bot.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
