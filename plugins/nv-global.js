ece64e4a7bd4827ce4a9737b9fd2b5073c16ce6d
Cawna.sex <104955231+TiagoKl@users.noreply.github.com>
2022-09-27 16:28:06 -0500

    𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -7,10 +7,11 @@ let vn = './audio/bot.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
 
-if (!chat.isBanned && chat.audios && m.text.match(/(anadieleimporta|a nadie le importa)/gi)) {
-let vn = './media/dylan1.mp3'
+if (/^Buenas Tardes|BUENAS TARDES|BUENASTARDES|buenas tardes$/i.test(m.text) && chat.audios && !chat.isBanned) {
+let vn = './audio/Buenas-Tardes.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
+
    
 return !0 }
 export default handler
