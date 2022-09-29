c07e4c7f9cbf59e721f79a9307b0d6839131033d
Cawna.sex <104955231+TiagoKl@users.noreply.github.com>
2022-09-29 06:18:04 -0500

    𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -13,7 +13,7 @@ if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }    
-await conn.sendButton(m.chat, `${await this.getName(m.sender)} Que se supone que hacés, JAJAJAJA, tanpoco soy tan idiota para dejarte pasar ese enlace, ratas como tú deberían ir donde les hace falta atención, Hora de funarte, Adiós!!!!.\n\n${isBotAdmin ? '' : 'Dame admin para poder eliminar personas.'}`,tiago, ['𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死', 'Eliminado'], m)    
+await conn.sendButton(m.chat, `⚠️ ENLACE DETECTADO ⚠️\n\nLo siento mucho ${await this.getName(m.sender)}, pero los enlaces de otros grupos no están permitidos, Adiós!!.\n${isBotAdmin ? '' : 'Dame admin para poder eliminar personas.'}`,tiago, ['🟢ERES ÉL MEJOR CAWNA-BOT🟢', 'Eliminado'], m)    
 if (isBotAdmin && bot.restrict) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 } else if (!bot.restrict) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙰𝙳𝙾 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*')
