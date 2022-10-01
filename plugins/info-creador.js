f1e784c16be338653934fa42254a74813c951bb7
Cawna.sex <104955231+TiagoKl@users.noreply.github.com>
2022-10-01 16:51:11 -0500

    𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -2,14 +2,24 @@ let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
 let text = `
-ြ̷̷̵̷̵̵̵̰̱̬̥̝̠̲̲̲̲̭̯̬̙̈̈̀̚͞͞͞͞ ⩴━ᚖ̦̯ۖ╥━̸̷︪︩͠٭━̷̸̳┈꯭┈ị.liᚖ̷̸̦̳ۖ╥━̷̸⵿ּ̇࠘┈̸َُ̳ٓ┈꯭━ᚖ━̷ּ̦̳̇ٔ࣪࠘ြ̷̷̵̷̵̵̵̰̱̬̥̝̠̲̲̲̲̭̯̬̙̈̈̀̚͞͞͞͞ 
-*—◉ wa.me/51946352266
-ြ̷̷̵̷̵̵̵̰̱̬̥̝̠̲̲̲̲̭̯̬̙̈̈̀̚͞͞͞͞ ⩴━ᚖ̦̯ۖ╥━̸̷︪︩͠٭━̷̸̳┈꯭┈ị.liᚖ̷̸̦̳ۖ╥━̷̸⵿ּ̇࠘┈̸َُ̳ٓ┈꯭━ᚖ━̷ּ̦̳̇ٔ࣪࠘ြ̷̷̵̷̵̵̵̰̱̬̥̝̠̲̲̲̲̭̯̬̙̈̈̀̚͞͞͞͞ 
+𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死
+
+🪀NÚMERO DE MI CREADOR OFICIAL🪀
+
++51 946 352 266
+
+💡CUENTA DE TIKTOK💡
+
+@Cawna.Sex
+
+👾CUENTA DE INSTAGRAM👾
+
+@Cawna.Sex
 `.trim()   
 let buttonMessage= {
 'document': { url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD` },
 'mimetype': `application/${document}`,
-'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+'fileName': `𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死`,
 'fileLength': 99999999999999,
 'pageCount': 200,
 'contextInfo': {
@@ -19,19 +29,14 @@ let buttonMessage= {
 'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
 'mediaType': 2,
 'previewType': 'pdf',
-'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
+'title': '🌻🌟EL MEJOR BOT DE WHATSAPP 🌟🌻',
 'body': wm,
 'thumbnail': imagen1,
-'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA' }},
+'sourceUrl': 'https://www.tiktok.com/@cawna.sex' }},
 'caption': text,
 'footer': wm,
-'buttons':[
-{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙼𝙴𝙽𝚄'}, type: 1}, 
-{buttonId: `${usedPrefix}donar`, buttonText: {displayText: '𝙳𝙾𝙽𝙰𝚁'}, type: 1}],
 'headerType': 6 }
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-//let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;𝑩𝒓𝒖𝒏𝒐 𝑺𝒐𝒃𝒓𝒊𝒏𝒐 👑;;;\nFN:𝑩𝒓𝒖𝒏𝒐 𝑺𝒐𝒃𝒓𝒊𝒏𝒐 👑\nORG:𝑩𝒓𝒖𝒏𝒐 𝑺𝒐𝒃𝒓𝒊𝒏𝒐 👑\nTITLE:\nitem1.TEL;waid=5219996125657:+521 999 612 5657\nitem1.X-ABLabel:𝑩𝒓𝒖𝒏𝒐 𝑺𝒐𝒃𝒓𝒊𝒏𝒐 👑\nX-WA-BIZ-DESCRIPTION:[❗] ᴄᴏɴᴛᴀᴄᴛᴀ ᴀ ᴇsᴛᴇ ɴᴜᴍ ᴘᴀʀᴀ ᴄᴏsᴀs ɪᴍᴘᴏʀᴛᴀɴᴛᴇs.\nX-WA-BIZ-NAME:𝑩𝒓𝒖𝒏𝒐 𝑺𝒐𝒃𝒓𝒊𝒏𝒐 👑\nEND:VCARD`
-//await conn.sendMessage(m.chat, { contacts: { displayName: 'Bruno Sobrino 👑', contacts: [{ vcard }] }}, {quoted: m})
 const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
