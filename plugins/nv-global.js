18726a6fcdedeafbf512dce88143edc60c2e1c7b
Cawna.sex <104955231+TiagoKl@users.noreply.github.com>
2022-10-08 09:29:55 -0500

    𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -2,15 +2,15 @@ let handler = m => m
 handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
 
-if (/^Bot|BOT|Cawna|cawna|bot$/i.test(m.text) && chat.audios && !chat.isBanned) {
-let vn = './audio/bot.mp3'
-this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
+//if (/^Bot|BOT|Cawna|cawna|bot$/i.test(m.text) && chat.audios && !chat.isBanned) {
+//let vn = './audio/bot.mp3'
+//this.sendPresenceUpdate('recording', m.chat)   
+//this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
 
-if (/^Buenas Tardes|BUENAS TARDES|BUENASTARDES|buenas tardes$/i.test(m.text) && chat.audios && !chat.isBanned) {
-let vn = './audio/Buenas-Tardes.mp3'
-this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
+//if (/^Buenas Tardes|BUENAS TARDES|BUENASTARDES|buenas tardes$/i.test(m.text) && chat.audios && !chat.isBanned) {
+//let vn = './audio/Buenas-Tardes.mp3'
+//this.sendPresenceUpdate('recording', m.chat)   
+//this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
 
    
 return !0 }
