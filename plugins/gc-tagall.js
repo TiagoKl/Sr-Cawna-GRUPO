987ecdb2b551a1140f99cfd4b4889708dd2165c9
Cawna.sex <104955231+TiagoKl@users.noreply.github.com>
2022-10-08 01:15:59 -0500

    𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,7 +5,7 @@ throw false
 }
 let pesan = args.join` `
 let oi = `${pesan}`
-let teks = `░⃟ ⃟ ┊LISTA DE TODOS LOS USUARIOS₊˚.༄\n\n👤${oi}\n\n👤  USUARIOS DEL GRUPO \n`
+let teks = `░⃟ ⃟ ┊LISTA DE TODOS LOS USUARIOS₊˚.༄\n\n⊙ {oi}\n\n𝐋𝐈𝐃𝚵𝐑 ː\n     𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死\n              𝚂𝙾𝚈 𝙴𝙻 𝙼𝙴𝙹𝙾𝚁 𝙱𝙾𝚃 𝙳𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿\n────── · · · · · ──────\n`
 for (let mem of participants) {
 teks += `♯┊@${mem.id.split('@')[0]}\n`}
 teks += `❐│ Creador: Tiago  \n      ©     𝖯𝖱𝗢𝗚𝙍꯭Ꜳ𝗠𝖬Σ𝖱\n────── · · · · · ──────`
