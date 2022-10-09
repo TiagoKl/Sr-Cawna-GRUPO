129a75306a07ae3998fb856f7d419f1ae4127a24
Cawna.sex <104955231+TiagoKl@users.noreply.github.com>
2022-10-09 11:07:07 -0500

    𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -8,18 +8,20 @@ _muptime = await new Promise(resolve => { process.once('message', resolve)
 setTimeout(resolve, 1000) }) * 1000}
 let uptime = clockString(_uptime)
 let estado =`
-╔═[ *∆.𝑫𝑶𝑹𝑹𝑯∆𝑻-𝑩𝑶𝑻.∆* ]
-║ *➤ 𝙷𝙾𝙻𝙰 ${name}*
-║
-║ *=➢ミ🤖 𝙴𝚂𝚃𝙰𝙳𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 🤖彡*
-║ *=➢ 𝙱𝙾𝚃 𝙰𝙲𝚃𝙸𝚅𝙾 ✅*
-║ *=➢ 𝙱𝙾𝚃 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 ✅*
-║ *=➢ 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: ${uptime}*
-╚════════════════
+🟢. .⌲⿻ ESTADO DEL BOT  . . .
+
+    •│ BOT DE USO PÚBLICO 
+
+ • TIEMPO ACTIVO 
+       ${uptime}
+
+❐│ Creador: Tiago  
+      ©     𝖯𝖱𝗢𝗚𝙍꯭Ꜳ𝗠𝖬Σ𝖱
+────── · · · · · ──────
 `.trim()
 
-conn.sendHydrated(m.chat, estado, wm, picture, 'https://github.com/DIEGO-OFC/DORRAT-BOT-MD', '📎𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
-['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']
+conn.sendHydrated(m.chat, estado, wm, picture, 'https://api.whatsapp.com/send?phone=+5', null, null, [
+['🟢MENU PRINCIPAL🟢', '/menu']
 ], m)}
 
 handler.help = ['estado']
